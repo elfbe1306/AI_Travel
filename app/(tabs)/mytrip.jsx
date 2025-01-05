f86714d4c496aeb75adb656ad79d592410da551f
elfbe <huynhngocnhon2004@gmail.com>
2025-01-05 20:36:48 +0700

    Finish tourSelection tab
    Phần tạo tour (Loading) được comment để fix front end

--- app/(tabs)/mytrip.jsx
+++ app/(tabs)/mytrip.jsx
@@ -1,10 +1,30 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { useRouter } from 'expo-router'
 
 export default function MyTrip() {
+  const router = useRouter();
   return (
-    <View>
+    <View style={styles.container}>
       <Text>MyTrip</Text>
+      <TouchableOpacity 
+        onPress={() => {
+          router.push({
+            pathname: '/tourStart',
+            params: {docIdForEdit: 1736017684142}
+          })
+        }}
+      >
+        <Text>Nhan em di a iu</Text>
+      </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
