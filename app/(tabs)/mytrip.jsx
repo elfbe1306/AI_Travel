ead310a879a317582cf0254b4775a751adb2a504
kienntheprfs <kien.trantrung@hcmut.edu.vn>
2025-01-08 01:18:18 +0700

    mytrip fe and be

--- app/(tabs)/mytrip.jsx
+++ app/(tabs)/mytrip.jsx
@@ -1,30 +1,74 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { useRouter } from 'expo-router'
+import { View, Text, Image, TouchableOpacity, ScrollView, TouchableHighlight } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Colors } from '../../constants/Colors';
+import Feather from '@expo/vector-icons/Feather';
+import { styles } from '../../styles/mytrip_style';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { collection, getDocs, query, where, doc, setDoc, getDoc } from 'firebase/firestore';
+import { useLocalSearchParams, useRouter } from 'expo-router';
+import { db } from '../../configs/FireBaseConfig';
 
 export default function MyTrip() {
   const router = useRouter();
-  return (
-    <View style={styles.container}>
-      <Text>MyTrip</Text>
-      <TouchableOpacity 
-        onPress={() => {
-          router.push({
-            pathname: '/tourStart',
-            params: {docIdForEdit: 1736017684142}
-          })
-        }}
-      >
-        <Text>Nhan em di a iu</Text>
-      </TouchableOpacity>
-    </View>
-  )
-}
+  const [tours, setTours] = useState([]); // State để lưu trữ dữ liệu tour
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
+  // Hàm để lấy dữ liệu từ Firebase
+  const fetchTours = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, 'UserTrips')); // 'UserTrips' là tên collection trong Firestore
+      const toursData = [];
+      querySnapshot.forEach((doc) => {
+        toursData.push({ id: doc.id, ...doc.data() }); // Lấy dữ liệu và thêm vào mảng
+        toursData.push({ id: doc.id + 1, ...doc.data() }); // Lấy dữ liệu và thêm vào mảng
+      });
+      setTours(toursData); // Cập nhật state với dữ liệu mới
+    } catch (error) {
+      console.error('Error fetching tours: ', error);
+    }
+  };
+
+  // Sử dụng useEffect để gọi hàm fetchTours mỗi khi component được render
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  return (
+    <View style={{ backgroundColor: Colors.LIME_GREEN, flex: 1 }}>
+      <View style={styles.headerContainer}>
+        <View style={styles.firstHeaderContainer}>
+          <View style={styles.userNameBox}>
+            <View style={styles.imageBox}>
+              <Image source={require('../../assets/images/character.png')} style={styles.userImage} />
+            </View>
+            <Text style={styles.userName}>Doan Le Vy </Text>
+          </View>
+
+          <View style={styles.notificationButton}>
+            <Feather name="bell" size={30} color="black" />
+          </View>
+        </View>
+      </View>
+
+      <Text style={styles.tourTitle}>Tour của tôi</Text>
+      <View style={styles.bodyContainer}>
+        {/* <View style={styles.firstBodyContainer}>
+        </View> */}
+
+        {/* Thay đổi ScrollView từ ngang sang dọc */}
+        <ScrollView style={styles.tourScrollView} showsVerticalScrollIndicator={false}>
+          {tours.map(tour => (
+            <TouchableOpacity key={tour.id} style={styles.tourCard} onPress={() => router.push({ pathname: '/tourStart', params: { docIdForEdit: tour.id } })}>
+              <Image source={{ uri: tour.tripData.places_to_visit[0].image_url }} style={styles.tourImage} />
+              <Text style={styles.tourDestination}>{tour.Destination}</Text>
+              <Text style={styles.tourDate}>{new Intl.DateTimeFormat('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' }).format(new Date(tour.StartDate))}</Text>
+              <TouchableOpacity style={styles.tourMarker}>
+                <Feather name="bookmark" size={20} color="white" />
+              </TouchableOpacity>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
+      </View>
+    </View>
+  );
+}
---
+++ styles/mytrip_style.jsx
@@ -0,0 +1,151 @@
+import { StyleSheet, Dimensions } from 'react-native';
+import { Colors } from '../constants/Colors';
+
+const screenWidth = Dimensions.get('window').width;
+const cardWidth = screenWidth * 0.92; // Tăng kích thước card lên 6/7 chiều rộng màn hình
+const cardHeight = cardWidth;
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerContainer: {
+    // flex: 1,
+  },
+  bodyContainer: {
+    flex: 1,
+    alignItems: 'center',
+    paddingBottom: '20%',
+  },
+  firstHeaderContainer: {
+    gap: screenWidth - 300,
+    marginTop: '15%',
+    flexDirection: 'row',
+  },
+  userNameBox: {
+    backgroundColor: 'white',
+    paddingLeft: 5,
+    paddingRight: 30,
+    paddingVertical: 5,
+    borderRadius: 40,
+    marginLeft: '5%',
+    flexDirection: 'row',
+    width: 200,
+  },
+  imageBox: {
+    backgroundColor: 'pink',
+    borderRadius: 99,
+    height: 50,
+  },
+  userImage: {
+    top: -30,
+    width: 50,
+    height: 75,
+  },
+  userName: {
+    marginTop: 16,
+    marginLeft: 10,
+    fontFamily: 'nunito-bold',
+    textAlign: 'center',
+  },
+  notificationButton: {
+    padding: 15,
+    backgroundColor: 'white',
+    borderRadius: 99,
+  },
+  secondHeaderContainer: {
+    marginTop: '5%',
+    marginLeft: '7%',
+    position: 'relative', // Cần thiết để định vị hình ảnh calligraphy line
+  },
+  calligraphyLine: {
+    position: 'absolute',
+    bottom: -10, // Điều chỉnh vị trí dọc
+    right: '20%', // Điều chỉnh vị trí ngang
+    width: 150, // Điều chỉnh kích thước hình ảnh
+    height: 20, // Điều chỉnh kích thước hình ảnh
+    resizeMode: 'contain', // Đảm bảo hình ảnh không bị méo
+  },
+  secondHeaderContainerTitleText: {
+    fontFamily: 'nunito-bold',
+    fontSize: 30,
+  },
+  firstBodyContainer: {
+    flexDirection: 'row',
+    // justifyContent: 'space-between',
+    marginHorizontal: '5%',
+    // alignItems: 'center',
+  },
+  tourTitle: {
+    fontFamily: 'nunito-bold',
+    fontSize: 20,
+    color: Colors.DARK_GREEN,
+    marginLeft: '5%',
+    paddingVertical: '2%',
+  },
+  viewAllText: {
+    fontFamily: 'nunito',
+    fontSize: 12,
+    color: Colors.DARK_GREEN,
+  },
+  tourScrollView: {
+    paddingVertical: 10,
+  },
+  tourCard: {
+    backgroundColor: '#FFE68A',
+    borderRadius: 20,
+    marginHorizontal: 10,
+    width: cardWidth, // Sử dụng chiều rộng mới
+    height: cardHeight, // Giữ nguyên chiều cao hoặc điều chỉnh nếu cần
+    padding: 12,
+    marginBottom: 20, // Thêm khoảng cách giữa các card
+    // Hiệu ứng shadow để tạo cảm giác nổi
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 3,
+    // Border xung quanh card
+    borderWidth: 1,
+    borderColor: 'rgba(255, 255, 255, 0.1)',
+    // Giảm độ đậm của border ở bottom
+    borderBottomWidth: 0.5, // Giảm độ dày của border ở bottom
+    borderBottomColor: 'rgba(255, 255, 255, 0.2)', // Giảm độ đậm của border ở bottom
+  },
+  tourImage: {
+    width: '100%',
+    height: '75%',
+    borderRadius: 17,
+  },
+  tourDestination: {
+    fontFamily: 'Nunito',
+    fontSize: 18,
+    fontWeight: '700',
+    lineHeight: 24,
+    letterSpacing: 0.5,
+    color: '#1B1E28',
+    marginTop: 10,
+  },
+  tourDate: {
+    fontFamily: 'Nunito',
+    fontSize: 12,
+    fontWeight: '500',
+    lineHeight: 16,
+    letterSpacing: 0.5,
+    color: '#7D848D',
+  },
+  tourMarker: {
+    position: 'absolute',
+    top: 20,
+    right: 20,
+    backgroundColor: 'rgba(27, 30, 40, 0.2)',
+    width: 34,
+    height: 34,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backdropFilter: 'blur(30px)',
+  },
+});
